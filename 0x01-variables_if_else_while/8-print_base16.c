d2eb47897a08d6a729676652186e39f8c69ebb0e
Boluwatife Fasina <boluayofashina@gmail.com>
2023-10-06 17:06:26 -0700

    Done..

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Start
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = '0';
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+	c = 'a';
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
