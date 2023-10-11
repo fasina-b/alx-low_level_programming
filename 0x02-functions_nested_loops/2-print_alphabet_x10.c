9f273dcd33b067bc4bf2e1afb4d4f1878c9fd541
Boluwatife Fasina <boluayofashina@gmail.com>
2023-10-10 19:16:26 -0700

    2-dowm

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -16,3 +16,20 @@ void print_alphabet(void)
 		letter++;
 	}
 }
+
+/**
+ * print_alphabet_x10 - Print the alphabet ten times in lowercase
+ *
+ * Description: This function calls print_alphabet ten times, each followed
+ * by a newline character, to print the lowercase alphabet ten times.
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		print_alphabet();
+		_putchar('\n');
+	}
+}
