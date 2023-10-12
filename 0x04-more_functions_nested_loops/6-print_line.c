b6098b8b97b4ff7641fbf9142c111bb7d7a8a029
Boluwatife Fasina <boluayofashina@gmail.com>
2023-10-12 16:31:29 -0700

    Task 4,5,6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line of underscores in the terminal
+ * @n: the number of times the character _ should be printed
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+
+		_putchar('\n');
+	}
+}
