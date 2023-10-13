36d91da83d2ec93b28ae9886bfcf461cb539caee
Boluwatife Fasina <boluayofashina@gmail.com>
2023-10-12 17:08:54 -0700

    10

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - Entry point for finding the largest prime factor of a number
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long n = 612852475143;
+	long max_prime = -1;
+
+	long i;
+	for (i = 3; i <= sqrt(n); i += 2)
+	{
+		while (n % i == 0)
+		{
+			max_prime = i;
+			n = n / i;
+		}
+	}
+
+	if (n > 2)
+	{
+		max_prime = n;
+	}
+
+	printf("%ld\n", max_prime);
+	return (0);
+}
