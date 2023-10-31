4a539aabe1a94982535c52476d787336f52b9dc8
Boluwatife Fasina <boluayofashina@gmail.com>
2023-10-31 14:16:12 -0700

    0-0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,19 +9,25 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	unsigned int i;
-	char arr;
-
 	if (size == 0)
+	{
 		return (NULL);
+	}
 
-	char *arr = (char *)malloc(size * sizeof(char));
+	char *arr;
+	unsigned int i;
+
+	arr = (char *)malloc(size * sizeof(char));
 
 	if (arr == NULL)
+	{
 		return (NULL);
+	}
 
 	for (i = 0; i < size; i++)
+	{
 		arr[i] = c;
+	}
 
 	return (arr);
 }
